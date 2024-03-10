229650e777490c8e91fd07ef63917e4ff9a7718b
YehShihYa <161419610+YehShihYa@users.noreply.github.com>
2024-03-10 16:51:29 +0800

    Add files via upload

--- Week3/PracticeWeek3.R
+++ Week3/PracticeWeek3.R
@@ -1,3 +1,11 @@
-library(ggplot2)
-plotly::ggplotly(p)
+#Example 1 Code：
+ggplot(data=)+
+  geom_point(mapping=aes(fill=COT (顏色分類), x=, y=))
 
+#Example 2  Code：
+ggplot(Data=df)+
+  geom_point(mapping=aes(x= Slen, y= Swid , fill= species))+
+  geom_abline(data=, mapping=aes (sloop=s,intercept=c,color=species ))#data=()：指定已分類好
+
+
+plotly::ggplotly
